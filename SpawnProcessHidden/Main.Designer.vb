ac46eaa992701bde2cc1005863fa46ddadc5fecd
asheroto <github@asheroto.com>
2022-07-03 01:29:26 -0500

    Restructure, add SpawnProcessHidden project

---
+++ SpawnProcessHidden/Main.Designer.vb
@@ -0,0 +1,60 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Main
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Main))
+        Me.TextBox = New System.Windows.Forms.TextBox()
+        Me.SuspendLayout()
+        '
+        'TextBox
+        '
+        Me.TextBox.Location = New System.Drawing.Point(14, 16)
+        Me.TextBox.Margin = New System.Windows.Forms.Padding(4)
+        Me.TextBox.Multiline = True
+        Me.TextBox.Name = "TextBox"
+        Me.TextBox.ReadOnly = True
+        Me.TextBox.Size = New System.Drawing.Size(796, 184)
+        Me.TextBox.TabIndex = 0
+        Me.TextBox.Text = resources.GetString("TextBox.Text")
+        '
+        'Main
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(7.0!, 17.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(823, 212)
+        Me.Controls.Add(Me.TextBox)
+        Me.Font = New System.Drawing.Font("Segoe UI", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
+        Me.Margin = New System.Windows.Forms.Padding(4)
+        Me.MaximizeBox = False
+        Me.MinimizeBox = False
+        Me.Name = "Main"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "SpawnProcessHidden"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents TextBox As TextBox
+End Class
